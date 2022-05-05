d681f0b24cbb1d74bba82315410150b7945a53d1
SvetlanaZt <svetlana.zt23@gmail.com>
2022-05-05 16:11:18 +0300

    dz

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+ 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const LOCAL_KEY = "videoplayer-current-time";
 
+player.on('timeupdate', onPlay)
+player.on('play', updateTime)
+
+const onPlay = function() {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem(LOCAL_KEY, seconds);
+    });
+};
+
+const updateTime = function () {
+    const saveTime = localStorage.getItem(LOCAL_KEY)
+    player.setCurrentTime(saveTime).then(function (seconds) {
+        console.log(seconds)
+    }).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+        }
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+const STORAGE_KEY = "feedback-form-state";
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmitBtn);
+
+onShowSaveValue()
+
+const formData = {};
+
+function onInput(evt) {
+    const mail = evt.target.value;
+    const message = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({ mail, message }))
+}
+function onSubmitBtn(evt) {
+    evt.preventDefault();
+
+    formData[evt.target.email.name] = evt.target.email.value
+    formData[evt.target.message.name] = evt.target.message.value
+    console.log(formData)
+// console.log(evt.target.email.name)
+// console.log(evt.target.email.value)
+
+    evt.currentTarget.reset()
+    localStorage.removeItem(STORAGE_KEY)
+
+
+}
+function onShowSaveValue() {
+    const getLocal = localStorage.getItem(STORAGE_KEY)
+    const getPars = JSON.parse(getLocal)
+
+    if (getPars) {
+        email.value = getPars.mail;
+        message.value = getPars.message;
+    }
+}
